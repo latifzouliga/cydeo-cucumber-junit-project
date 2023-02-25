9cebf91042c025f47d3466016b63f584cb5208fd
Abdellatif Zouliga <zuuliga@gmail.com>
2023-02-25 10:02:55 -0500

    new updates

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,14 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)                             // turns the class into runnable class
 @CucumberOptions(   //
         plugin = {
-                "json:target/cucumber.json",
+                "json:target/cucumber.json",         // json reports, useful for jenkins
                 "html:target/cucumber-reports.html", // html report for passed and failed Tests
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 "rerun:target/rerun.txt"             // storing tailed text for rerun
         },
         features = "src/test/resources/features",    // looking for Scenarios in feature folder
         glue = "com/cydeo/step_definitions",         // looking for implementation of feature files in step defs folder
-        tags = "",                            // specifying which test(s) needs to be run
+        tags = "",
+        // specifying which test(s) needs to be run
         publish = true,                              // generating a link for sharing html report
         dryRun = false                               // generating snippets when we change the value to true
 )
